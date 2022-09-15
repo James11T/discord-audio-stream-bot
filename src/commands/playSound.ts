b644f6c5df3f9208bf88e360de9e66540875bc2f
James <jamail090999@gmail.com>
2022-09-15 21:20:50 +0100

    Added playsound command

---
+++ src/commands/playSound.ts
@@ -0,0 +1,41 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+import { audioPlayer, audioResource } from "../audio";
+import type { CommandInteraction } from "discord.js";
+import fs from "fs";
+import { createAudioResource } from "@discordjs/voice";
+
+const command = new SlashCommandBuilder()
+  .setName("playsound")
+  .setDescription("Play a sound file from bot")
+  .addStringOption((option) =>
+    option
+      .setName("filename")
+      .setDescription("Filename of sound to play")
+      .setRequired(true)
+  ) as SlashCommandBuilder;
+
+const handler = async (interaction: CommandInteraction) => {
+  const file = interaction.options.get("filename")?.value;
+  if (file === undefined) return;
+
+  const fullName = `static/${file}`;
+  const exists = fs.existsSync(fullName);
+
+  const resource = createAudioResource(fullName);
+  audioPlayer.play(resource);
+
+  if (!exists) {
+    await interaction.reply({
+      content: "File does not exist",
+      ephemeral: true
+    });
+    return;
+  }
+
+  await interaction.reply({
+    content: `Playing \`${file}\``,
+    ephemeral: false
+  });
+};
+
+export { command, handler };
